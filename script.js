20279a23ce250a68bee4a6ceec32308f117da895
Admin <admin@MacBook-Air-de-Admin.local>
2024-01-29 23:22:43 +0100

    Profile card

---
+++ script.js
@@ -0,0 +1,8 @@
+//const myElement = document.getElementById('myElement');
+
+/*
+setting css properties for images
+
+const myImage = document.getElementById('myImage');
+myImage.style.borderRadius = '50%'
+myImage.style.textAlign = 'center';*/
